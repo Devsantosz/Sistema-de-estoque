87bf574cecb9bb95d594c4db53db2b461a1be058
Guilherme Luz <161989907+Devsantosz@users.noreply.github.com>
2026-01-30 17:42:37 -0300

    Sistema de controle de estoque usando python e SQlite

---
+++ app.py
@@ -0,0 +1,61 @@
+import os
+import sqlite3
+import uuid
+import bcrypt
+from flask import Flask, render_template, request, redirect, session, abort
+
+app = Flask(__name__)
+
+
+DB_PATH = "data/app.db"
+
+
+def get_db():
+    os.makedirs("data", exist_ok=True)
+    conn = sqlite3.connect(DB_PATH)
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
+def init_db():
+    conn = get_db()
+    cur = conn.cursor()
+
+
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS products (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            quantity INTEGER NOT NULL DEFAULT 0
+        );
+        """)
+
+
+
+    conn.commit()
+    conn.close()
+
+@app.route("/")
+def home():
+    con = get_db()
+    cur = con.cursor()
+    cur.execute("SELECT name, quantity from products order by id")
+    con.close()
+    return render_template("dashboard.html")
+
+@app.route("/add")
+def adicionar():
+    name = request.form["nome"].strip()
+    qty = int(request.form["qty"])
+
+    con = get_db()
+    cur = con.cursor
+    cur.execute("INSERT INTO products (name, quantity) VALUES (?, ?)", (name, qty))
+
+    con.commit()
+    con.close
+    return redirect("/")
+
+if __name__ == "__main__":
+    init_db()
+    app.run(debug=True)
