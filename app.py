acc207d0e24ff5d12c81f9a1c9c8d2f1f2bacae4
Guilherme Luz <161989907+Devsantosz@users.noreply.github.com>
2026-01-31 00:38:34 -0300

    Config- adicionando botao de remover e fazendo conexao com BD

--- app.py
+++ app.py
@@ -5,8 +5,6 @@ import bcrypt
 from flask import Flask, render_template, request, redirect, session, abort
 
 app = Flask(__name__)
-
-
 DB_PATH = "data/app.db"
 
 
@@ -20,18 +18,13 @@ def get_db():
 def init_db():
     conn = get_db()
     cur = conn.cursor()
-
-
     cur.execute("""
         CREATE TABLE IF NOT EXISTS products (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             name TEXT NOT NULL,
             quantity INTEGER NOT NULL DEFAULT 0
         );
-        """)
-
-
-
+    """)
     conn.commit()
     conn.close()
 
@@ -39,21 +32,33 @@ def init_db():
 def home():
     con = get_db()
     cur = con.cursor()
-    cur.execute("SELECT name, quantity FROM products ORDER BY id DESC")
+    cur.execute("SELECT id, name, quantity FROM products ORDER BY id DESC")
     products = cur.fetchall()
     con.close()
     return render_template("dashboard.html", products=products)
 
 
+
 @app.post("/add")
 def adicionar():
     name = request.form["name"].strip()
-    qty = int(request.form["qty"])
+    if not name:
+        return redirect("/")
+
+    qty = max(0, int(request.form["qty"]))
 
     con = get_db()
     cur = con.cursor()
     cur.execute("INSERT INTO products (name, quantity) VALUES (?, ?)", (name, qty))
+    con.commit()
+    con.close()
+    return redirect("/")
 
+@app.post("/remove/<int:id>")
+def remover(id):
+    con = get_db()
+    cur = con.cursor()
+    cur.execute("DELETE FROM products WHERE id = ?", (id,))
     con.commit()
     con.close()
     return redirect("/")
